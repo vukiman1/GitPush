c049564dca3b551ebac386a43b9d15f851683ba4
KimAn <anvu734@gmail.com>
2023-06-08 21:27:14 +0700

    init project

---
+++ push/main.js
@@ -0,0 +1,11 @@
+//alert("Reset ok");
+
+function App() {
+  return (
+    <div id="wraper">
+      <button onClick={() => alert("ók")}>Click here</button>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
